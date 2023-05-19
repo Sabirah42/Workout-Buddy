a3d43ab78a1b27cd1bbc5f9184c8c56add2ca588
Sabirah <sabirah.miah.ahammed@gmail.com>
2023-05-19 20:09:17 +0100

    added home route

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 // server.js is the entry file for the backend
 const express = require('express')
 
-// create tthe express app
+// create the express app
 const app = express();
+
+// routes
+app.get('/', (req, res) => {
+    res.json({mssg: 'Welcome to the app'})
+})
 
 // listen for requests
 app.listen(4000, () => {
