cc0c4b3310c78a6a06706420dfc4e17acbf4d01d
Sabirah <sabirah.miah.ahammed@gmail.com>
2023-05-24 16:25:38 +0100

    added delete and update controllers

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -3,14 +3,14 @@
 const Workout = require('../models/workoutModel')
 const mongoose = require('mongoose')
 
-// get all workouts
+// GET ALL WORKOUTS
 const getWorkouts = async (req, res) => {
     const workouts = await Workout.find({}).sort({createdAt: -1})
 
     res.status(200).json(workouts)
 }
 
-// get a single workout
+// GET A SINGLE WORKOUT
 const getWorkout = async (req, res) => {
     // gets changeable id from the params
     const { id } = req.params
@@ -27,7 +27,7 @@ const getWorkout = async (req, res) => {
     res.status(200).json(workout)
 }
 
-// create a new workout
+// CREATE A NEW WORKOUT
 const createWorkout = async (req, res) => {
     const {title, load, reps} = req.body
 
@@ -40,10 +40,47 @@ const createWorkout = async (req, res) => {
     }
 }
 
-// delete a workout
+// DELETE A WORKOUT
+const deleteWorkout = async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'No such workout found'})
+    }
+    // where '_id' from MongoDB = 'const id' above
+    const workout = await Workout.findOneAndDelete({_id: id})
+
+    if (!workout) {
+        return res.status(404).json({error: 'No such workout found'})
+    } 
+
+    res.status(200).json(workout)
+}
+
+// UPDATE A WORKOUT
+const updateWorkout = async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'No such workout found'})
+    }
+    // where '_id' from MongoDB = 'const id' above
+    const workout = await Workout.findOneAndUpdate({_id: id}, {
+        // 'req.body' is an object, so you need to spread these using '...'
+        ...req.body
+    })
+
+    if (!workout) {
+        return res.status(404).json({error: 'No such workout found'})
+    } 
+
+    res.status(200).json(workout)
+}
 
 module.exports = {
     getWorkouts,
     getWorkout,
-    createWorkout
+    createWorkout,
+    deleteWorkout,
+    updateWorkout
 }
