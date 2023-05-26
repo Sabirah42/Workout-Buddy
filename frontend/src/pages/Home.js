c8e6f16b2ca3c1efbffca7fcb161d7aa71ccd1e0
Sabirah <sabirah.miah.ahammed@gmail.com>
2023-05-26 15:13:08 +0100

    homepage returning all workouts

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,29 @@
+import { useEffect, useState} from 'react'
+
 const Home = () => {
+    const [workouts, setworkouts] = useState(null)
+
+// empty array as second argument ensures that this onlly fires on first render
+    useEffect(() => {
+        const fetchWorkouts  = async () => {
+            const response = await fetch('http://localhost:4000/api/workouts')
+            const json = await response.json()
+
+            if (response.ok) {
+                setworkouts(json)
+            }
+        }
+
+        fetchWorkouts()
+    }, [])
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+                    <p key={workout._id}>{workout.title}</p>
+                ))}
+            </div>
         </div>
     )
 }
