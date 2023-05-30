e31fc91705f192cdc4f43ce9dbc23e425c3c4619
Sabirah <sabirah.miah.ahammed@gmail.com>
2023-05-30 15:27:47 +0100

    added code notes

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -2,9 +2,14 @@ import { createContext, useReducer } from "react";
 
 export const WorkoutsContext = createContext();
 
+// could have also used useState hook to pass state to child
+// first argument is the current state
+// second argument is the object passed into 'dispatch' function - has type and payload properties
 export const workoutsReducer = (state, action) => {
+    // type describes the state change
     switch (action.type) {
         case 'SET_WORKOUTS':
+            // payload represents any data we need to make that change
             return {
                 workouts: action.payload
             }
@@ -17,13 +22,14 @@ export const workoutsReducer = (state, action) => {
     }
 }
 
+// component that will wrap everything that needs context
 export const WorkoutsContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(workoutsReducer, {
         workouts: null
     })
 
     return (
-        // component that will wrap everything that needs context. Children represents <App />
+        //  'children' represents <App />
         <WorkoutsContext.Provider value={{state, dispatch}}>
             { children }
         </WorkoutsContext.Provider>
