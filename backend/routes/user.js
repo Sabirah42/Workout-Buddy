d7806066fd42297d57c00ccc8b33ac51e01768d7
Sabirah <sabirah.miah.ahammed@gmail.com>
2023-06-02 12:22:30 +0100

    setup user Controller and connected to user Route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,12 +1,15 @@
 const express = require('express')
 
+// conroller functions
+const { loginUser, signupUser } = require('../controllers/userController')
+
 const router = express.Router()
 
 // login route
-router.post('/login', () => {})
+router.post('/login', loginUser)
 
 // signup route
-router.post('/signup', () => {})
+router.post('/signup', signupUser)
 
 
 
