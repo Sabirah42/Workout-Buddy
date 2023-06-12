4c8ba58034ab2975904e653d52112b15dd753d12
Sabirah <sabirah.miah.ahammed@gmail.com>
2023-06-12 22:16:16 +0100

    setup password hashing in userModel

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const Schema = mongoose.Schema
 
@@ -13,6 +14,32 @@ const userSchema = new Schema({
         required: true
     }
 })
+
+//static signup method
+userSchema.statics.signup = async (email, password) => {
+
+    const exists = await this.findOne({ email })
+
+    if (exists) {
+        throw Error('Email already in use')
+    }
+
+    // bcrypt uses salt - a random string of characters that get added to thhe password before hashing.
+    // stops hackers from password matching
+
+    // generate salt - argument is the number of characters that gets added. The longer the number, the harder for the hacker,
+    // but also the longer it takes for a user to sign up, so you need a happy medium. Usual number is 10
+    const salt = await bcrypt.genSalt(10)
+
+    // then hash the password
+    const hash = await bcrypt.hash(password, salt)
+
+    // create the document
+    const user = await this.create({ email, password: hash })
+
+    // return the user so the function can be called elsewhere
+    return user
+}
 
 
 module.exports = mongoose.model('User', userSchema)
