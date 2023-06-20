52be51ac8dcda79a5a78853a2bf17c28d2435f91
Sabirah <sabirah.miah.ahammed@gmail.com>
2023-06-20 20:20:29 +0100

    fixed signup error

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,18 +7,31 @@ const createToken = (_id) => {
 
 // login user
 const loginUser = async (req, res) => {
-    res.json({mssg: 'login user'})
+    const {email, password} = req.body
+
+    try {
+        // email and password is taken from request body (line 10)
+        const user = await User.login(email, password)
+
+        // create  a token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+
 }
 
 // signup user
 const signupUser = async (req, res) => {
-    const { email, password } = req.body
+    const {email, password} = req.body
 
     try {
         const user = await User.signup(email, password)
 
         // create  a token
-        const token = createToken(_id)
+        const token = createToken(user._id)
 
         res.status(200).json({email, token})
     } catch (error) {
