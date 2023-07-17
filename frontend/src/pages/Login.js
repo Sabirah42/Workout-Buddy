63fda22b5daff57fd15a907a3eb36cb0d0441a65
Sabirah <sabirah.miah.ahammed@gmail.com>
2023-07-17 13:17:40 +0100

    fixed login and signup form errors

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -22,7 +22,7 @@ const Login = () => {
             <label>Password:</label>
             <input
                 type="password"
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={(e) => setPassword(e.target.value)}
                 value={password} 
             />
             <button>Log in</button>
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -21,7 +21,7 @@ const Signup = () => {
             <label>Password:</label>
             <input
                 type="password"
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={(e) => setPassword(e.target.value)}
                 value={password} 
             />
             <button>Sign up</button>
